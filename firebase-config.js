56d34d0b38a2ccf0eb5ad16d95bcd11c4ad8fcc8
PowerhouseFoundry <james.paylor@westsilc.org>
2026-05-18 22:00:50 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,19 +1,17 @@
-/*
-  Powerhouse CV Builder Firebase config
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-  1. Create a Firebase project.
-  2. Add a Web App in Firebase.
-  3. Copy the firebaseConfig object here, replacing the placeholder values below.
-  4. In Firestore, create/use a collection called: cvRecords
-
-  Until this file is completed, the app will still work using browser-only storage.
-*/
-
-window.POWERHOUSE_FIREBASE_CONFIG = {
-  apiKey: "PASTE_API_KEY_HERE",
-  authDomain: "PASTE_PROJECT_ID.firebaseapp.com",
-  projectId: "PASTE_PROJECT_ID",
-  storageBucket: "PASTE_PROJECT_ID.appspot.com",
-  messagingSenderId: "PASTE_SENDER_ID",
-  appId: "PASTE_APP_ID"
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAzzUYImfadYdJflChdQpoYUkrc5kuJ18U",
+  authDomain: "cv-builder-1d923.firebaseapp.com",
+  projectId: "cv-builder-1d923",
+  storageBucket: "cv-builder-1d923.firebasestorage.app",
+  messagingSenderId: "302690174030",
+  appId: "1:302690174030:web:5995b8c551801ee7772f1f"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
